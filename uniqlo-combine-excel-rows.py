c1e51a49838fabb0d6d7f2bcab4238a626c7cf98
Fendy Lieanata <fendy@Fendys-MacBook-Pro.local>
2023-04-29 21:29:53 +0700

    fixed

--- uniqlo-combine-excel-rows.py
+++ uniqlo-combine-excel-rows.py
@@ -7,11 +7,9 @@ pathToCsv = os.getcwd()+"/dropCsvHere/"
 
 # user input
 userinput_wantSum = input("Do you want to sum a particular column? Press 1 for yes, press anything for no.")
-
 if userinput_wantSum == '1':
     userinput_columnNumber = input("Which column number do you want to sum? If first row, press 1.")
-
-
+    sumOfColumn = 0
 
 # Use glob to get a list of all CSV files in the directory
 csv_files = glob.glob(pathToCsv + '*.csv')
@@ -24,17 +22,19 @@ for csv_file in csv_files:
     df = pd.read_csv(csv_file)
     dfs.append(df)
 
-# Calculate the sum
-if userinput_wantSum == '1':
-    column_name = df.columns[int(userinput_columnNumber)]
-    sum_of_column = df.iloc[:, int(userinput_columnNumber)-1].sum()
-
-    print("The sum of column {}: {}".format(column_name, sum_of_column))
+    # Add the sum
+    if userinput_wantSum == '1':
+        sumOfColumn += df.iloc[:, int(userinput_columnNumber)-1].sum()
 
 # Concatenate all dataframes into one
 combined_df = pd.concat(dfs, ignore_index=True)
+
+# Drop any empty rows from the dataframe
+combined_df.dropna(inplace=True)
 
 # Write the combined dataframe to a new CSV file
 combined_df.to_csv('combined.csv', index=False)
 
 print('combined.csv is created. All hail lord Fendy')
+if userinput_wantSum == '1':
+    print("The sum: {}".format(sumOfColumn))
