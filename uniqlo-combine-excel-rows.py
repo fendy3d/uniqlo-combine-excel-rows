29e604ef85413ea44a89ed61da5fb0d1b77a1baf
Fendy Lieanata <fendy@Fendys-MacBook-Pro.local>
2023-04-29 20:49:54 +0700

    add a sum

--- uniqlo-combine-excel-rows.py
+++ uniqlo-combine-excel-rows.py
@@ -5,6 +5,12 @@ import os
 # Path to the directory containing CSV files to be combined
 pathToCsv = os.getcwd()+"/dropCsvHere/"
 
+# user input
+userinput_wantSum = input("Do you want to sum a particular column? Press 1 for yes, press anything for no.")
+userinput_columnNumber = input("Which column number do you want to sum? If first row, press 1.")
+
+
+
 # Use glob to get a list of all CSV files in the directory
 csv_files = glob.glob(pathToCsv + '*.csv')
 
@@ -15,6 +21,11 @@ dfs = []
 for csv_file in csv_files:
     df = pd.read_csv(csv_file)
     dfs.append(df)
+
+# Calculate the sum
+if userinput_wantSum == '1':
+    sum_of_column = df.iloc[:, int(userinput_columnNumber)-1].sum()
+    print("The sum of column {}: {}".format(userinput_columnNumber, sum_of_column))
 
 # Concatenate all dataframes into one
 combined_df = pd.concat(dfs, ignore_index=True)
